f12401c822f5c7b2598323c1de4f814fab0ce3d5
seunggs <seunggs@gmail.com>
2014-04-21 22:21:10 -0400

    passes milestone 2

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,4 +1,8 @@
 
 class DiscountCalculator(object):
 	def calculate(self, total, discount_amount, discount_type):
-		return 10
+		percentage_discount = float(discount_amount) / 100
+		discount = percentage_discount * float(total)
+		return discount
+
+		
